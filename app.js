12c1c118ebe1f44fa2f99c8a1fd3645c8701a545
samhuang1992 <bob60117@gmail.com>
2021-12-23 17:35:16 +0800

    fix: modify port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const routes = require('./routes')
 const app = express()
 // 如果在 Heroku 環境則使用 process.env.PORT
 // 否則為本地環境，使用 3000 
-const port = process.env.port || 3000
+const PORT = process.env.PORT  || 3000
 // 載入mongoose.js
 require('./config/mongoose')
 
@@ -16,6 +16,6 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`App.js is running on http://locoalhost${port}`)
+app.listen(PORT, () => {
+  console.log(`App.js is running on http://locoalhost${PORT}`)
 })
