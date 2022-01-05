7b544a1f70f93efd70070b967812fc5b2a7c62be
samhuang1992 <bob60117@gmail.com>
2022-01-05 21:45:02 +0800

    feat: add logout feature

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,7 @@ module.exports = app => {
   // 設定本地登入策略
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     USER.findOne({ email }) 
-      .then(user => {
+    .then(user => {
         if(!user){
           return done(null, false, { message: 'That email is not register.' })
         }
@@ -26,10 +26,10 @@ module.exports = app => {
   // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
     console.log(user)
-    done,(null, user.id)
+    done(null, user.id)
   })
   passport.deserializeUser((id, done) => {
-    USER.findOne(id)
+    USER.findById(id)
     .lean()
     // 查詢DB運作正常,回傳查找的結果user
     .then(user => done(null, user))
@@ -37,4 +37,31 @@ module.exports = app => {
     .catch(err => done(err, null))
   })
 }
+// module.exports = app => {
+//   app.use(passport.initialize())
+//   app.use(passport.session())
 
+//   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+//     USER.findOne({ email })
+//       .then(user => {
+//         if (!user) {
+//           return done(null, false, { message: 'That email is not registered!' })
+//         }
+//         if (user.password !== password) {
+//           return done(null, false, { message: 'Email or Password incorrect.' })
+//         }
+//         return done(null, user)
+//       })
+//       .catch(err => done(err, false))
+//   }))
+
+//   passport.serializeUser((user, done) => {
+//     done(null, user.id)
+//   })
+//   passport.deserializeUser((id, done) => {
+//     USER.findById(id)
+//       .lean()
+//       .then(user => done(null, user))
+//       .catch(err => done(err, null))
+//   })
+// }  
